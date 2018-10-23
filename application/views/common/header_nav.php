561b81aa541bf077f4bf34eb0f1b95e53a0b1114
Sobcat <771954909@qq.com>
2018-10-23 17:08:10 +0800

    update

--- application/views/common/header_nav.php
+++ application/views/common/header_nav.php
@@ -1,12 +1,14 @@
 <header>
-  <div class="logo"></div>
-  <nav class="menu">
-    <ul>
-      <li><a href="http://">首页</a></li>
-      <li><a href="http://">公司简介</a></li>
-      <li><a href="http://">产品中心</a></li>
-      <li><a href="http://">在线留言</a></li>
-      <li><a href="http://">联系我们</a></li>
-    </ul>
-  </nav>
+  <div class="header-contect common-width clearfix">
+    <div class="header-logo">这里是 logo 企业名称</div>
+    <nav class="header-menu">
+      <ul>
+        <li><a href="http://">网站首页</a></li>
+        <li><a href="http://">公司简介</a></li>
+        <li><a href="http://">产品中心</a></li>
+        <li><a href="http://">在线留言</a></li>
+        <li><a href="http://">联系我们</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
