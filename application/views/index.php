7b6b1992ac83f6d4df27f580d193e2ea4871c3e7
Sobcat <771954909@qq.com>
2018-11-14 17:06:06 +0800

    update

--- application/views/index.php
+++ application/views/index.php
@@ -74,19 +74,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</span>
 			</div>
 			<ul class="product-list clearfix product-list-01">
-				<li><img src="" alt=""><span></span></li>
-				<li><img src="" alt=""><span></span></li>
-				<li><img src="" alt=""><span></span></li>
-				<li><img src="" alt=""><span></span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
 			</ul>
 			<ul class="product-list clearfix product-list-02">
-				<li><img src="" alt=""><span></span></li>
-				<li><img src="" alt=""><span></span></li>
-				<li><img src="" alt=""><span></span></li>
-				<li><img src="" alt=""><span></span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
+				<li><img src="/assets/images/kl.jpg" alt=""><span>名字</span></li>
 			</ul>
 	</div>
 	<!-- 产品展示 end-->
-	
+	<!-- 在线留言 -->
+	<div class="common-width online-message">
+		<div class="title clearfix">
+					<h1 class="">在线留言</h1>
+					<span  class="">
+						<a href="">MORE</a>
+					</span>
+		</div>
+		<p>如果对我们的产品有订购需求或者有什么建议，欢迎在线留言，我们真诚为您服务！</p>
+		<div class="clearfix">
+			<form action="" class="">
+				<h3>标题</h3>
+				<div><input type="text" name='mes-title'></div>
+				<h3>手机<strong>*</strong></h3>
+				<div><input type="text" name='mes-phone'></div>
+				<h3>内容</h3>
+				<div><textarea name="mes-text" id=""  style="resize:none;"></textarea></div>
+				<div><button>提交</button></div>
+			</form>
+			<div class="om-img-container">
+				<img src="/assets/images/lyt1.jpg" alt="">
+				<img src="/assets/images/lyt1.jpg" alt="">
+			</div>
+		</div>
+	</div>
+	<!-- 在线留言 end -->
 </body>
 </html>
